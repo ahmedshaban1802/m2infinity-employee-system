70693d1b9f069d9b99256b7002ed453b3098a1dd
Ahmed Shaban <ahmedshaban1802@users.noreply.github.com>
2026-07-16 23:25:33 +0300

    add leave request button + all v5 fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  async rewrites() {
+    return [{ source: "/", destination: "/index.html" }];
+  },,
   typescript: {
     ignoreBuildErrors: true,
   },
